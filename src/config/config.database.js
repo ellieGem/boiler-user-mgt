86911c3cec2a949b6cd09a180db3166c7a12efee
Dennis Machuwhich bash <dennis.machu@outlook.com>
2020-02-05 18:17:46 +0000

    added database config

---
+++ src/config/config.database.js
@@ -0,0 +1,53 @@
+//require mongoose module
+var mongoose = require('mongoose');
+
+//require chalk module to give colors to console text
+var chalk = require('chalk');
+
+// Database setup
+const mainDB = process.env.DEV_DB;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+	return (mainDB = process.env.PRO_DB);
+}
+
+var connected = chalk.bold.cyan;
+var error = chalk.bold.yellow;
+var disconnected = chalk.bold.red;
+var termination = chalk.bold.magenta;
+
+//export this function and imported by server.js
+module.exports = function() {
+	mongoose.Promise = global.Promise;
+
+	mongoose.connect(mainDB, {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	});
+
+	mongoose.connection.on('connected', function() {
+		console.log(connected('Mongoose default connection is open'));
+	});
+
+	mongoose.connection.on('error', function(err) {
+		console.log(
+			error('Mongoose default connection has occured ' + err + ' error')
+		);
+	});
+
+	mongoose.connection.on('disconnected', function() {
+		console.log(disconnected('Mongoose default connection is disconnected'));
+	});
+
+	process.on('SIGINT', function() {
+		mongoose.connection.close(function() {
+			console.log(
+				termination(
+					'Mongoose default connection is disconnected due to application termination'
+				)
+			);
+			process.exit(0);
+		});
+	});
+};
